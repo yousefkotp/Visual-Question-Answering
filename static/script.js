6db76829b1be806a7445a7c83011baeac68a6bce
Yousef <yousefkotp@outlook.com>
2023-06-23 04:11:15 +0300

    Configured HTML, CSS and JavaScript

---
+++ static/script.js
@@ -0,0 +1,25 @@
+function submitForm() {
+    var form = document.getElementById("predict-form");
+    var formData = new FormData(form);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/predict");
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        var resultDiv = document.getElementById("result");
+        // We will recieve object as a string, so we need to parse it to JSON
+        var objectReceived = JSON.parse(xhr.responseText);
+
+        var answer = objectReceived.answer;
+        var answer_type = objectReceived.answer_type;
+        var answerability = objectReceived.answerability;
+        console.log(answer);
+
+        resultDiv.innerHTML = "<p>Answer: " + answer + "</p>" +
+                                "<p>Answer type: " + answer_type + "</p>" +
+                                "<p>Answerability: " + answerability + "</p>";
+        } else {
+        alert("An error occurred while trying to predict the answer.");
+      }
+    };
+    xhr.send(formData);
+  }
